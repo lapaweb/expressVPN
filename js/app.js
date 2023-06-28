0421a925b1ead59cb5caf143f76d92aa985363f5
lapaweb <lapa-34@ya.ru>
2023-06-28 14:48:37 +0300

    modal js

--- js/app.js
+++ js/app.js
@@ -6,6 +6,31 @@ window.addEventListener('scroll', function() {
   }
 })
 
-// $(window).on("scroll", function() {
-//   $(селектор header).toggleClass("active", $(this).scrollTop() > $(window).height());
+
+// const myModal = new HystModal({
+//   linkAttributeName: "data-hystmodal",
+//   // настройки (не обязательно), см. API
 // });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+   let callBackButton = document.getElementById('callback-button');
+   let modal1 = document.getElementById('modal-1');
+   let closeButton = modal1.getElementsByClassName('modal__close-button')[0];
+ 
+  callBackButton.onclick = function () {
+    modal1.classList.add('modal_active');
+  }
+ 
+  closeButton.onclick = function () {
+    modal1.classList.remove('modal_active');
+  }
+
+  modal1.onmousedown = function (e) {
+    let target = e.target;
+    let modalContent = modal1.getElementsByClassName('modal__content')[0];
+    if (e.target.closest('.' + modalContent.className) === null) {
+      this.classList.remove('modal_active');
+    }
+  };
+});
